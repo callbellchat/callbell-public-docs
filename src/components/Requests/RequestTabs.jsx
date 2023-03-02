04f114a17c39a1d70dd91619e329a3353ac53be1
Qwertic <vodaregi@gmail.com>
2023-03-02 16:44:52 +0100

    enh: create generic code tabs component

---
+++ src/components/Requests/RequestTabs.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Tabs from "@theme/Tabs";
+import TabItem from "@theme/TabItem";
+import CodeBlock from "@theme/CodeBlock";
+
+export default function RequestTabs(props) {
+  const snippets = [
+    {
+      key: "bash",
+      component: require(`../../snippets/curl/${props.endpoint}/_${props.request}.mdx`),
+      value: "cURL",
+    },
+    {
+      key: "node",
+      component: require(`../../snippets/node/${props.endpoint}/_${props.request}.mdx`),
+      value: "Node",
+    },
+    {
+      key: "ruby",
+      component: require(`../../snippets/ruby/${props.endpoint}/_${props.request}.mdx`),
+      value: "Ruby",
+    },
+    {
+      key: "go",
+      component: require(`../../snippets/go/${props.endpoint}/_${props.request}.mdx`),
+      value: "Go",
+    },
+    {
+      key: "php",
+      component: require(`../../snippets/php/${props.endpoint}/_${props.request}.mdx`),
+      value: "PHP",
+    },
+    {
+      key: "python",
+      component: require(`../../snippets/python/${props.endpoint}/_${props.request}.mdx`),
+      value: "Python",
+    },
+  ];
+  console.log(snippets);
+
+  return (
+    <>
+      <Tabs groupId="programming-language">
+        {snippets.map((snippet, id) => {
+          const Component = snippet.component;
+
+          return (
+            <TabItem
+              key={id}
+              value={snippet.value}
+              label={snippet.value}
+              default
+            >
+              <CodeBlock
+                language={snippet.key}
+                title="request.sh"
+                showLineNumbers
+              >
+                <Component.default />
+              </CodeBlock>
+            </TabItem>
+          );
+        })}
+      </Tabs>
+    </>
+  );
+}
