eb3b44b630c88582f7c4515b388b92d83f253337
Qwertic <vodaregi@gmail.com>
2023-03-03 18:49:49 +0100

    add searcbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,10 @@ const config = {
   organizationName: "callbellchat", // Usually your GitHub org/user name.
   projectName: "callbell-public-docs", // Usually your repo name.
 
+  plugins: [
+    [require.resolve("@cmfcmf/docusaurus-search-local"), { indexBlog: false }],
+  ],
+
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
